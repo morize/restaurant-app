a1829c09ad7b7fbb2e9adcdf86ed06c693427c6c
Mauriccio <mauricemr@outlook.com>
2022-03-01 13:16:25 +0100

    checkpoint graphql test

---
+++ server/src/server.js
@@ -0,0 +1,30 @@
+const { join } = require('path');
+const express = require('express');
+
+const { graphqlHTTP } = require('express-graphql');
+const { loadFilesSync } = require('@graphql-tools/load-files');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+
+const typesArray = loadFilesSync(join(__dirname, '**/*.graphql'));
+
+const schema = makeExecutableSchema({
+    typeDefs: typesArray,
+});
+
+const root = {
+    description: 'Red Shoe',
+    price: 42.2,
+};
+
+const app = express();
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: schema,
+        rootValue: root,
+        graphiql: true,
+    })
+);
+
+app.listen(3000, () => console.log('started'));
