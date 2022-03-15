57aaaf2b82a11e30faa1d484fd07e3d2c28bf84d
morize <mauricemr@outlook.com>
2022-03-15 08:36:21 +0100

    backend/create new mongodb user when user logs in for the first time, if user exists return lookup db and return document

--- server/src/setup/passport.js
+++ server/src/setup/passport.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 const { Strategy } = require('passport-google-oauth20');
 
 const { CLIENT_ID, CLIENT_SECRET } = require('../utils/config');
+const { getUserByGoogleId, addNewUser } = require('../data/users/users.model');
 
 function verifyAuthCallback(accessToken, refreshToken, profile, done) {
   done(null, profile);
@@ -16,12 +17,19 @@ function setupGooglePassportStrategy() {
 
   passport.use(new Strategy(AUTH_OPTIONS, verifyAuthCallback));
 
-  passport.serializeUser((user, done) => {
-    done(null, user.id);
+  passport.serializeUser(async (user, done) => {
+    const existingUser = await getUserByGoogleId(user.id);
+
+    if (!existingUser) {
+      const newUserDoc = await addNewUser(user.name.givenName, user.id);
+      done(null, newUserDoc);
+    } else {
+      done(null, existingUser);
+    }
   });
 
-  passport.deserializeUser((id, done) => {
-    done(null, id);
+  passport.deserializeUser((obj, done) => {
+    done(null, obj);
   });
 }
 
