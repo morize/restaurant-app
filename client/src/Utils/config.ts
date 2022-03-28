679703060eac23c19e9204589b00fdc61ce0f804
morize <mauricemr@outlook.com>
2022-03-28 13:04:24 +0200

    frontend/modified build script to build in server public folder

--- client/src/Utils/config.ts
+++ client/src/Utils/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  API_URL: 'http://localhost:3000',
-  API_PROD_URL: 'https://localhost:443',
+  API_URL: 'https://localhost',
   // API_DEV_URL: 'http://localhost:3000',
+  // API_DEV_URL: 'https://localhost',
 };
